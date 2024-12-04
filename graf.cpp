6b59bd53e99631beef9b2a3c0293a3a95a2b28ee
alkausarhapis <alkausarhapis@gmail.com>
2024-12-04 23:32:02 +0700

    Feat: Setengah Main & Setengah function

--- graf.cpp
+++ graf.cpp
@@ -1,1 +1,32 @@
 #include "graf.h"
+
+void CreateGraph(Graph &G) {
+	Start(G) = Null;
+}
+
+Addr_Node AlokasiNode(Infotype_Node infoNode) {
+	Addr_Node P = new Elmt_Node;
+	Info(P) = infoNode;
+	FirstEdge(P) = Null;
+	NextNode(P) = Null;
+	return P;
+}
+
+Addr_Edge AlokasiEdge(Infotype_Edge infoEdge) {
+	Addr_Edge P = new Elmt_Edge;
+	Info(P) = infoEdge;
+	NextEdge(P) = Null;
+	return P;
+}
+
+void AddNewNode(Graph &G, Addr_Node P) {
+	if (Start(G) == Null) {
+		Start(G) = P;
+	} else {
+		Addr_Node Q = Start(G);
+		while (NextNode(Q) != Null) {
+			Q = NextNode(Q);
+		}
+		NextNode(Q) = P;
+	}
+}
